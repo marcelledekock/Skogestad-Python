c05ce31b86cd500252e76a3a167e66768865eec9
marcelledekock <marcelledk@mintek.co.za>
2016-06-10 07:51:18 +0200

    completed TODO

--- Example_04_10.py
+++ Example_04_10.py
@@ -2,16 +2,6 @@ from __future__ import print_function
 import sympy
 import utils
 
-s = sympy.Symbol('s')
-M = sympy.Matrix([[(s - 1)*(s + 2), 0, (s - 1)**2],
-                      [-(s + 1)*(s + 2), (s - 1)*(s + 1),  (s - 1)*(s + 1)]])
-G = (1/((s + 1)*(s + 2)*(s - 1)))*M
-
-poles = utils.poles(G)
-
-print("The poles of the system are = ",poles)
-
-# TODO: This should work correctly
 s = utils.tf([1, 0], 1)
 M = utils.mimotf([[(s - 1) * (s + 2), 0, (s - 1) ** 2],
                    [-(s + 1) * (s + 2), (s - 1) * (s + 1), (s - 1) * (s + 1)]])
